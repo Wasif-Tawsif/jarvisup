132f9406c5b667c8bb2af08efc02e1c951e8de6f
Wasif-Tawsif <101320651+Wasif-Tawsif@users.noreply.github.com>
2022-03-10 20:03:04 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -173,7 +173,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Mirror-Leech-Bot Help',
+        title='Jarvis Bot Help',
         content=help_string_telegraph,
     )["path"]
 
